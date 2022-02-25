875322968f40f7faa219ae742fb4abb5fa2b91f4
/\be| /\nand <90404118+AbelXAnand13@users.noreply.github.com>
2022-02-24 19:16:09 -0800

    Initial Commit

---
+++ searchAVector/main.cpp
@@ -0,0 +1,43 @@
+//Abel Anand
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+//if ur looking at this you should make all your programs time complex smh imao
+using namespace std;
+
+struct info {
+    string name;
+    int age;
+    int height;
+    int weight;
+    string phoneNumber;
+};
+
+int main() {
+    cout << "hulo";
+//    vector <info> ppls;
+//
+//    ifstream in;
+//    in.open("/Users/Abel/Desktop/Cpp/searchAVector/searchAVector/savIn.txt");
+//
+//    string temp;
+//
+//    if (!in.is_open()) {
+//        cout << "File not found\n";
+//    }
+//
+//    info person;//temporary instance of the struct
+//
+//    while (getline(in, temp)) {
+//        getline(in, person.name);
+//        person.age = stoi(temp);
+//        person.height = stoi(temp);
+//        person.weight = stoi(temp);
+//        getline(in, person.phoneNumber);
+//        ppls.push_back(person);
+//    }
+//    for (int i = 0; i < ppls.size(); i++) {
+//        cout << ppls[i].name << " " << ppls[i].age << " " << ppls[i].height <<  " " << ppls[i].weight <<  " " << ppls[i].phoneNumber << endl;
+//    }
+}
