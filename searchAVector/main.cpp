9e73b91f24bbb8cdf4cda00e095b04bf26a8588c
/\be| /\nand <90404118+AbelXAnand13@users.noreply.github.com>
2022-02-27 17:10:50 -0800

    FinalCommit1 said what i had to and did what i did
    very hard at first, but easier as it went on(because i overthought)

--- searchAVector/main.cpp
+++ searchAVector/main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <fstream>
 #include <string>
+#include <stdio.h>
 //if ur looking at this you should make all your programs time complex smh imao
 using namespace std;
 
@@ -15,29 +16,105 @@ struct info {
 };
 
 int main() {
-    cout << "hulo";
-//    vector <info> ppls;
-//
-//    ifstream in;
-//    in.open("/Users/Abel/Desktop/Cpp/searchAVector/searchAVector/savIn.txt");
-//
-//    string temp;
-//
-//    if (!in.is_open()) {
-//        cout << "File not found\n";
-//    }
-//
-//    info person;//temporary instance of the struct
-//
-//    while (getline(in, temp)) {
-//        getline(in, person.name);
-//        person.age = stoi(temp);
-//        person.height = stoi(temp);
-//        person.weight = stoi(temp);
-//        getline(in, person.phoneNumber);
-//        ppls.push_back(person);
-//    }
-//    for (int i = 0; i < ppls.size(); i++) {
-//        cout << ppls[i].name << " " << ppls[i].age << " " << ppls[i].height <<  " " << ppls[i].weight <<  " " << ppls[i].phoneNumber << endl;
-//    }
+    vector <info> ppls;
+
+    ifstream in;
+    in.open("/Users/Abel/Desktop/Cpp/searchAVector/searchAVector/savIn.txt");
+
+    string temp;
+
+    if (!in.is_open()) {
+        cout << "File not found\n";
+    }
+
+    info person;
+    //temporary instance of the struct
+
+    while (getline(in, person.name, ',')) {
+        getline(in, temp, ',');
+        person.age = stoi(temp);
+        getline(in, temp, ',');
+        person.height = stoi(temp);
+        getline(in, temp, ',');
+        person.weight = stoi(temp);
+        getline(in, person.phoneNumber);
+        ppls.push_back(person);
+    }
+    in.close();
+    while (1) {
+        int type;
+        string input;
+        cout << "What would you like to search for\n1. Name\t2. Age\t3. Height\t4. Weight\t5. Phone Number\t0. QUIT\n";
+        cin >> type;
+        if (type == 0) {
+            exit(0);
+        }
+        cout << "Enter the value you want to search for\n";
+        cin >> input;
+        int times = 0;
+        switch (type) {
+            case 1:
+                for(int i = 0; i < input.length(); i++) {
+                    input[i] = tolower(input[i]);
+                }
+                for (int i = 0; i < ppls.size(); i ++) {
+                    if (ppls[i].name == input) {
+                        cout << "Age = " << ppls[i].age << "\tHeight = " << ppls[i].height << "\t\tWeight = " << ppls[i].weight << "\tPhone = " << ppls[i].phoneNumber << endl << endl;
+                        times++;
+                    }
+                }
+                if (times == 0) {
+                    cout << "Couldnt find anyone\n\n";
+                }
+                break;
+            case 2:
+                for (int i = 0; i < ppls.size(); i++) {
+                    if (ppls[i].age == stoi(input)) {
+                        cout << ppls[i].name << " ";
+                        times++;
+                    }
+                }
+                if (times == 0) {
+                    cout << "Couldnt find anyone";
+                }
+                cout << endl << endl;
+                break;
+            case 3:
+                for (int i = 0; i < ppls.size(); i++) {
+                    if (ppls[i].height == stoi(input)) {
+                        cout << ppls[i].name << " ";
+                        times++;
+                    }
+                }
+                if (times == 0) {
+                    cout << "Couldnt find anyone";
+                }
+                cout << endl << endl;
+                break;
+            case 4:
+                for (int i = 0; i < ppls.size(); i++) {
+                    if (ppls[i].weight == stoi(input)) {
+                        cout << ppls[i].name << " ";
+                        times++;
+                    }
+                }
+                if (times == 0) {
+                    cout << "Couldnt find anyone";
+                }
+                cout << endl << endl;
+                break;
+            case 5:
+                for (int i = 0; i < ppls.size(); i++) {
+                    if (ppls[i].age == stoi(input)) {
+                        cout << ppls[i].name << " ";
+                        times++;
+                    }
+                }
+                if (times == 0) {
+                    cout << "Couldnt find anyone";
+                }
+                cout << endl << endl;
+                break;
+        }
+    }
 }
